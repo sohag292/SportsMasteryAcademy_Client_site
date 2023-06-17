85c25d819388d41bb030baad83331e08785a5c0f
sohag292 <sohagshah571@gmail.com>
2023-06-17 11:37:14 +0600

    instractur some code changes

--- src/Pages/Instructors/Instructors.jsx
+++ src/Pages/Instructors/Instructors.jsx
@@ -8,17 +8,17 @@ const Instructors = () => {
     const cardBackgroundClass = isDark ? "bg-indigo-200" : "bg-base-100";
     return (
         <div>
-            <h2 className='text-xl md:text-3xl font-bold text-center my-4 text-primary '>Instructors</h2>
+            <h2 className='text-xl md:text-3xl font-bold text-center my-4 text-success '>All Instructors : {instructors.length}</h2>
             <div className='grid grid-cols-3 w-11/12 mx-auto mt-10'>
                 {
                     instructors?.map(ins =>
-                        <div className={`card card-compact w-96 bg-base-100  ${cardBackgroundClass} shadow-xl`}>
+                        <div className={`card card-compact my-6 w-96 bg-base-100  ${cardBackgroundClass} shadow-xl`}>
                             <figure><img className='h-72 w-full object-cover' src={ins?.photoURL} /></figure>
                             <div className="card-body">
                                 <h2 className="card-title">Name: {ins?.name}</h2>
                                 <p>Email: {ins?.email}</p>
                                 <div className="card-actions justify-end">
-                                    <button className={`btn ${isDark ? "bg-dark-purple text-indigo-100 hover:text-dark-purple border-0 shadow-sm shadow-white" : "btn-primary"}`}>See Classes</button>
+                                    <button className={`btn ${isDark ? "bg-dark-purple text-indigo-100 hover:text-dark-purple border-0 shadow-sm shadow-white" : "btn-success"}`}>See Classes</button>
                                 </div>
                             </div>
                         </div>
